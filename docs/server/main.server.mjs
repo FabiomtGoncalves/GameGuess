2f56146842e51701db2771a2fe0d8444e3b0ca0b
Fábio Gonçalves <fabio.mt.goncalves@gmail.com>
2024-03-20 18:07:53 +0000

    ng build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JV36WE6W.mjs";import"./chunk-3YB2IP6D.mjs";import"./chunk-2U2O45XV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-CFHBEJ6M.mjs";import"./chunk-UKWCFNS2.mjs";import"./chunk-XEOA2VSI.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
